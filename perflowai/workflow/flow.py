5e9d547b8d4ff17e8aa2f53aec06a153cd2be00d
yuyangJin <yuyuanjin47@gmail.com>
2025-02-02 15:41:09 +0800

    [Feat] #5 Update FlowNode class

--- perflowai/workflow/flow.py
+++ perflowai/workflow/flow.py
@@ -1,3 +1,12 @@
+'''
+@module workflow
+'''
+
+'''
+@class FlowNode
+A FlowNode is a node in a flow graph.
+'''
+
 class FlowNode:
     def __init__(self, name, id, inputs, outputs):
         self.m_name = name
@@ -6,19 +15,39 @@ class FlowNode:
         self.m_outputs = outputs
 
     def __str__(self):
-        return f"FlowNode({self.name})"
+        return f"FlowNode({self.m_name})"
 
     def run(self, *args, **kwargs):
         return self.func(*args, **kwargs)
 
+'''
+@class FlowGraph
+A FlowGraph is a diagram of tasks.
+'''
+
 class FlowGraph:
     def __init__(self):
 
-        # 
-        self.m_nodes = list()
+        '''
+        Dict<int, FlowNode> m_nodes
+        The key is the node id, the value is the node.
+        '''
+        self.m_nodes = dict()
         
-        # 
+        '''
+        Dict<int, List<int>>> m_edges
+        The first int is the source node id, 
+        the second List is the destination node ids.
+        '''
         self.m_edges = dict()
+
+    def add_node(self, node):
+        self.m_nodes[node.m_id] = node
+    
+    def add_edge(self, src_node, dst_node):
+        if src_node.m_id not in self.m_edges:
+            self.m_edges[src_node.m_id] = []
+        self.m_edges[src_node.m_id].append(dst_node.m_id)
 
     ''' 
     check the graph to ensure the output of 
