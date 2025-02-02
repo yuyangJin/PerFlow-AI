25815b0e75cee1483dcd5a1019d2ca574316aece
yuyangJin <yuyuanjin47@gmail.com>
2025-02-02 15:42:45 +0800

    [Test] #5 Add a unit test for FlowNode class

---
+++ tests/units_tests/flownode_test.py
@@ -0,0 +1,13 @@
+'''
+test FlowNode class
+'''
+
+from perflowai.workflow.flow import FlowNode
+
+def test_FlowNode():
+    node = FlowNode("test", 0, [], [])
+    assert node.m_name == "test"
+    assert node.m_id == 0
+    assert node.m_inputs == []
+    assert node.m_outputs == []
+    assert str(node) == "FlowNode(test)"
