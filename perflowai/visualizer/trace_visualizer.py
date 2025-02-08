406d5bb1c125569200234e811dbb3c8bbd2b3a14
shuixirui <2021302111210@whu.edu.cn>
2025-02-08 18:09:25 +0800

    [Feat] #7 Update TraceVisiualizer class

--- perflowai/visualizer/trace_visualizer.py
+++ perflowai/visualizer/trace_visualizer.py
@@ -2,9 +2,30 @@
 @package perflowai.visualizer.trace_visualizer
 '''
 
-
+import numpy as np
+import drawsvg as draw
 from perflowai.workflow.flow import FlowNode
 
+from perflowai.core.event import Event, FwdBwdEvent, EventType
+from perflowai.core.trace import Trace
+
+ENABLE_BORDER = True
+ENABLE_BATCH_ID = True
+ENABLE_EDGE_BLUR = False
+SCALE_FACTOR = 2
+S = SCALE_FACTOR
+
+# TIME_PER_UNIT = 300 // SCALE_FACTOR
+TIME_PER_UNIT = 4000 // SCALE_FACTOR
+
+GREYSCALE_WEIGHTS = np.array([0.299, 0.587, 0.114])
+
+TAIL = 50
+BORDER_SIZE = 1
+SPAN_HEIGHT = SCALE_FACTOR * 10
+FONT_SIZE = SCALE_FACTOR * 10
+TITLE_WIDTH = SCALE_FACTOR * 60
+CENTER_TITLE_HEIGHT = SPAN_HEIGHT * 6
 
 '''
 @class TraceVisiualizer
@@ -16,7 +37,150 @@ class TraceVisiualizer(FlowNode):
 
     def visualize(self):
         # visualize the trace
-        pass
+        #    Title Width              Data_width
+        #   ------------------------------------------------
+        #   |          |                                   |
+        #   |   device |                                   |
+        #   |   title  |              data                 |  data_height
+        #   |          |                                   |
+        #   |          |                                   |
+        #   ------------------------------------------------
+        #   |                    info                      |  info_height
+        #   ------------------------------------------------
+        #   |                  svg Title                   |  title_height
+        #   ------------------------------------------------
+        
+        ndevs = self.trace.m_ndevs
+
+        title_width = TITLE_WIDTH
+
+        first_time = min(min([e.m_timestamp for e in dev_evs]) for dev_evs in self.trace.m_events.values())
+        last_time = max(max([e.m_timestamp + e.m_duration for e in dev_evs]) for dev_evs in self.trace.m_events.values())
+        max_chk = max(max([e.m_chunk_id if hasattr(e, 'm_chunk_id') else 0 for e in dev_evs]) for dev_evs in self.trace.m_events.values())
+        data_width = (last_time - first_time + TIME_PER_UNIT - 1) // TIME_PER_UNIT + TAIL
+        
+        data_height = SPAN_HEIGHT * ndevs + BORDER_SIZE * (ndevs + 1)
+
+        color_text_row_height = int(SPAN_HEIGHT * 1.6)
+        color_text_height = int(SPAN_HEIGHT * 1.6) + BORDER_SIZE
+        info_height = SPAN_HEIGHT + int(SPAN_HEIGHT * 1.6) + 3 * BORDER_SIZE
+
+        title_height = CENTER_TITLE_HEIGHT
+
+        canvas_width = title_width + data_width
+        canvas_height = data_height + info_height + title_height
+
+        d = draw.Drawing(canvas_width, canvas_height, origin="top-left")
+
+        exist_type = []
+
+        def get_color(typ, chk = 0):
+            #COLOR_VALUE_MAP
+            c = np.array([0, 0, 0])
+            if typ == EventType.FWD:
+                c = np.array([57, 122, 242])
+            elif typ == EventType.BWD:
+                c = np.array([62, 181, 191])
+            elif typ == EventType.WGT:
+                c = np.array([41, 137, 64])
+            elif typ == EventType.COMM:
+                c = np.array([255, 217, 102])
+            elif typ == EventType.OPRT:
+                c = np.array([255, 217, 102])
+            else:
+                raise ValueError("Type must be an instance of EventType")
+            
+            #lighten_color
+            lightness = chk * 70 / max_chk
+            
+            R = c[0]
+            G = c[1]
+            B = c[2]  
+
+            # 计算变换后的RGB值  
+            R_prime = int(R + (255 - R) * (lightness / 100))  
+            G_prime = int(G + (255 - G) * (lightness / 100))  
+            B_prime = int(B + (255 - B) * (lightness / 100))  
+
+            # 确保RGB值在0到255之间  
+            R_prime = max(0, min(255, R_prime))  
+            G_prime = max(0, min(255, G_prime))  
+            B_prime = max(0, min(255, B_prime))  
+
+            return f"#{hex(R_prime)[2:]}{hex(G_prime)[2:]}{hex(B_prime)[2:]}"
+
+        def plot_line(sy, sx, ey, ex, width=None):
+            d.append(draw.Line(sx, sy, ex, ey, stroke='black', stroke_width=width or BORDER_SIZE))
+        
+        def plot_rect(sy, sx, h, w, color):
+            d.append(draw.Rectangle(sx, sy, w, h, fill=color, shape_rendering="geometricPrecision"))
+
+        def plot_rect_frame(sy, sx, h, w):
+            d.append(draw.Rectangle(sx, sy, w, h, fill="none", stroke='black', stroke_width=BORDER_SIZE))
+        
+        def plot_text(y, x, text, anchor="middle", font_scale=1, fill='black'):
+            font_size = FONT_SIZE * font_scale
+            tl = len(text) * font_size // 2
+            d.append(draw.Text(
+                text, font_size,
+                x,
+                # Magic 3 to make it vertical center
+                y + font_size - 3,
+                textLength=tl, lengthAdjust='spacing',
+                text_anchor=anchor,
+                font_family="Times New Roman",
+                fill=fill,
+                # font_style="oblique",
+                # font_family="Computer Modern Roman",
+            ))
+
+        def plot_arrow(start_y, start_x, width, thickness=2):
+            b = thickness * (SCALE_FACTOR // 2)
+            plot_line(start_y, start_x, start_y, start_x + width, b)
+            plot_line(start_y, start_x + width, start_y - 3*b, start_x + width - 3*b)
+            plot_line(start_y, start_x + width, start_y + 3*b, start_x + width - 3*b)
+
+        def draw_device_title(oy, ox):
+            for i in range(ndevs):
+                h = i * SPAN_HEIGHT + (i + 1) * BORDER_SIZE
+                plot_text(oy + h, ox + 6 * SCALE_FACTOR, "Device {}".format(i), "left")
+        
+        def draw_data(oy, ox):
+            for it, dev_events in self.trace.m_events.items():
+                h = it * SPAN_HEIGHT + (it + 1) * BORDER_SIZE
+                for e in dev_events:
+                    start = BORDER_SIZE + (e.m_timestamp - first_time) // TIME_PER_UNIT
+                    duration = e.m_duration // TIME_PER_UNIT
+                    center = start + duration // 2
+                    if e.m_type not in exist_type:
+                        exist_type.append(e.m_type)
+                    plot_rect(oy + h, ox + start, SPAN_HEIGHT, duration, get_color(e.m_type, (e.m_chunk_id if hasattr(e, 'm_chunk_id') else 0)))
+                    plot_rect_frame(oy + h - BORDER_SIZE, ox + start, SPAN_HEIGHT + BORDER_SIZE, duration)
+                    plot_text(oy + h + SPAN_HEIGHT / 5, ox + center, e.m_name, font_scale=0.6, fill='black')
+                plot_line(oy + h + SPAN_HEIGHT, ox, oy + h + SPAN_HEIGHT, ox + data_width -1)
+
+        def draw_info(oy, ox, types):
+            div = 2 + len(types)
+            for it, typ in enumerate(types):
+                start = canvas_width // div * (it + 1) 
+                img_block_w = 25 * SCALE_FACTOR
+                text_block_w = 28 * SCALE_FACTOR
+                plot_rect(oy + color_text_height + BORDER_SIZE, ox + start, SPAN_HEIGHT, img_block_w, get_color(typ))
+                plot_rect_frame(oy + color_text_height, ox + start, SPAN_HEIGHT + BORDER_SIZE, img_block_w)
+                plot_text(oy + color_text_height, ox + start + text_block_w, str(f"{typ}".split('.')[1][0:3]).lower(), "left")
+
+            plot_text(oy, 6 * SCALE_FACTOR, "Time", "left")
+            plot_arrow(oy + SPAN_HEIGHT // 2 + BORDER_SIZE + 1, 65 * SCALE_FACTOR, 50 * SCALE_FACTOR)
+            
+        def draw_svg_title(oy, ox):
+            plot_text(data_height + info_height + CENTER_TITLE_HEIGHT / 4, canvas_width / 2, "Title", "middle", 2)
+
+        draw_device_title(0, 0)
+        draw_data(0, title_width)
+        draw_info(data_height, 0, exist_type)
+        draw_svg_title(data_height + info_height, 0)
+
+        d.save_svg("trace.svg")
 
     def run(self):
         self.visualize()
