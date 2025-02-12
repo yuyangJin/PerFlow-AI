6a7b5be9a2615f09567dd5ead6e26edd23d51bee
yuyangJin <yuyuanjin47@gmail.com>
2025-02-13 00:19:16 +0800

    [Feat] #18 Update the PPGraph class: implement main functions

--- perflowai/parallel/pipeline_parallel/ppgraph.py
+++ perflowai/parallel/pipeline_parallel/ppgraph.py
@@ -2,7 +2,30 @@
 @module pipeline parallel
 '''
 
-from perflowai.core.trace import Trace
+from ...core.trace import Trace
+from ...core.event import EventType, FwdBwdEvent, NoneTimestamp
+
+from dataclasses import dataclass
+
+'''
+PipeCostConfig
+'''
+@dataclass
+class PipeCostConfig:
+    fwd_time: int = 1
+    bwd_time: int = 1
+    wgt_time: int = 1
+    comm_time: int = 1
+    fwd_mem: float = 2
+    bwd_mem: float = -1
+    wgt_mem: float = -1
+
+    def __post_init__(self):
+        assert type(self.fwd_time) is int
+        assert type(self.bwd_time) is int
+        assert type(self.wgt_time) is int
+        assert type(self.comm_time) is int
+        assert self.fwd_mem + self.bwd_mem + self.wgt_mem == 0
 
 '''
 @class PPGraph
@@ -10,38 +33,136 @@ A PPGraph is a pipeline trace.
 An event is a node in the PPGraph, and it must be FwdBwdEvent.
 '''
 class PPGraph(Trace):
-    def __init__(self, nstages, nmicrobatches, nchunks, n):
-        # Basic information for pipeline parallelism
+    def __init__(self, nstages, nmicrobatches, nchunks, cost_config=None):
+        ''' 
+        Basic information for pipeline parallelism 
+        '''
         self.m_nstages = nstages
         self.m_nmicrobatches = nmicrobatches
         self.m_nchunks = nchunks
+        self.m_cost_config = cost_config
         
-        self.m_edges = dict()
+        '''
+        Dict<int, FwdBwdEvent> m_nodes
+        The first key is event_id, the value is the corresponding event.
+        '''
+        self.m_nodes = dict()
 
-    def get_event_id(self):
+        '''
+        Dict<int, List<int>> m_out_edges/m_in_edges
+        m_out_edges: The first key is src id, the value is the list of dst id.
+        m_in_edges: The first key is dst id, the value is the list of src id.
+        '''
+        self.m_in_edges = dict()
+        self.m_out_edges = dict()
+
+        '''
+        '''
+        self.event_types = [EventType.FWD, EventType.BWD]
+
+    def get_event_id(self, event_type, stage_id, microbatch_id, chunk_id = 0):
         '''
         Calculate the node id based on the stage, microbatch, and chunk
         '''
+        return event_type.value * self.m_nchunks * self.m_nstages * self.m_nmicrobatches + \
+               chunk_id * self.m_nstages * self.m_nmicrobatches + \
+               stage_id * self.m_nmicrobatches + \
+               microbatch_id
+
+    def get_event_types(self):
+        return self.event_types
+
+    def get_nstages(self):
+        return self.m_nstages
+
+    def get_nchunks(self):
+        return self.m_nchunks
+
+    def get_nmicrobatches(self):
+        return self.m_nmicrobatches
+
+    def add_node(self, event_type, stage_id, microbatch_id, duration, chunk_id = 0):
+        '''
+        Get event id
+        '''
+        event_id = self.get_event_id(event_type, stage_id, microbatch_id, chunk_id)
         
-        pass
+        '''
+        Get event name
+        '''
+        event_name = str(event_type) + '-' + str(microbatch_id) + '-' + str(chunk_id)
+        
+        '''
+        Create a new fwd/bwd event
+        '''
+        fwdbwd_event = FwdBwdEvent(id = event_id, 
+                            type = event_type, 
+                            name=event_name, 
+                            timestamp = NoneTimestamp, 
+                            duration = duration, 
+                            stage_id = stage_id, 
+                            microbatch_id = microbatch_id, 
+                            chunk_id = chunk_id)
+        
+        '''
+        Insert the created event into the node list
+        '''
+        self.m_nodes[event_id] = fwdbwd_event
+
+        return id
 
     def generate_nodes(self):
-        pass
+        if self.m_cost_config == None:
+            raise ValueError("Cost config should be setup at initialization.")
 
-    def add_edge(self, src, dst):
-        pass
+        event_types = self.get_event_types()
+        n_stages = self.get_nstages()
+        n_microbatches = self.get_nmicrobatches()
+        n_chunks = self.get_nchunks()
+        for type in event_types:
+            for stage in range(n_stages):
+                for mb in range(n_microbatches):
+                    for chk in range(n_chunks):
+                        if type == EventType.FWD:
+                            self.add_node(type, stage, mb, self.m_cost_config.fwd_time, chk)
+                        elif type == EventType.BWD:
+                            self.add_node(type, stage, mb, self.m_cost_config.bwd_time, chk)
+                        elif type == EventType.WGT:
+                            continue
+                        else:
+                            assert False
+
+    def add_edge(self, src_id, dst_id):
+        '''
+        Add out edges
+        '''
+        if src_id not in self.m_out_edges.keys():
+            self.m_out_edges[src_id] = set()
+        self.m_out_edges[src_id].add(dst_id)
+
+        '''
+        Add in edges
+        '''
+        if dst_id not in self.m_in_edges.keys():
+            self.m_in_edges[dst_id] = set()
+        self.m_in_edges[dst_id].add(src_id)
 
     def get_nodes(self):
         return self.m_nodes
 
-    def get_edges(self):
-        return self.m_edges
+    def get_in_edges(self):
+        return self.m_in_edges
+
+    def get_out_edges(self):
+        return self.m_out_edges
 
     def check(self):
-        for src in self.m_edges:
-            for dst in self.m_edges[src]:
-                assert dst in self.m_edges, f"node {dst} not found"
+        for src_id in self.m_out_edges:
+            for dst_id in self.m_out_edges[src_id]:
+                assert dst_id in self.m_nodes.keys(), f"node {dst_id} not found"
+        for dst_id in self.m_in_edges:
+            for src_id in self.m_in_edges[dst_id]:
+                assert src_id in self.m_nodes.keys(), f"node {src_id} not found"
 
-    def run(self, *args, **kwargs):
-        for node in self.m_nodes:
-            node.run(*args, **kwargs)
+    def output(self):
+        print(self.m_nodes)
