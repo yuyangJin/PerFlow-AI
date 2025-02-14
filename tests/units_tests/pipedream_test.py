bd719e4b572ddc0cee6eebb8cda0bb6682ede435
yuyangJin <yuyuanjin47@gmail.com>
2025-02-14 14:41:51 +0800

    [Test] #21 Add a unit test for PipeDreamGraph class

---
+++ tests/units_tests/pipedream_test.py
@@ -0,0 +1,36 @@
+'''
+test PipeDreamGraph class
+'''
+import pytest
+
+from perflowai.parallel.pipeline_parallel.ppgraph import PipeCostConfig
+from perflowai.parallel.pipeline_parallel.pipedream import PipeDreamGraph
+from perflowai.core.event import NoneTimestamp, EventType
+
+def test_GPipeGraph():
+
+    with pytest.raises(AssertionError):
+        g = PipeDreamGraph(4, 10, 2, cost_config = PipeCostConfig(
+            fwd_time = 5478,
+            bwd_time = 5806,
+            wgt_time = 3534
+        ))
+
+    g = PipeDreamGraph(4, 10, 1, cost_config = PipeCostConfig(
+        fwd_time = 5478,
+        bwd_time = 5806,
+        wgt_time = 3534
+    ))
+
+    g.build_graph()
+    
+    assert g.get_event_types() == [EventType.FWD, EventType.BWD]
+    assert g.get_nstages() == 4
+    assert g.get_nchunks() == 1
+    assert g.get_nmicrobatches() == 10
+
+    g.check()
+
+    for event in g.get_nodes().values():
+        assert event.get_timestamp() == NoneTimestamp
+
