e18b527e28bcceac8180e3ba9f78fc8c2d644ca7
shuixirui <2021302111210@whu.edu.cn>
2025-02-26 14:11:21 +0800

    [Refactor] #40 Update Filter&Merge classes and tests，import from relative path，add comments，derived from FlowNode

--- perflowai/trace_op/merge.py
+++ perflowai/trace_op/merge.py
@@ -1,18 +1,34 @@
-from perflowai.parallel.pipeline_parallel.ppgraph import PPGraph
+from ..parallel.pipeline_parallel.ppgraph import PPGraph
+# Import from relative path
+import copy
 
-class Merge():
+# derived from FlowNode.
+'''
+@class Merge
+A graph Merger
+'''
+class Merge(FlowNode):
     def __init__(self):
         pass
     
     def merge_pp(self, graph_list, edge_list):
+        '''
+        0. SETUP VARIABLES
+        '''
+        # get basic information
         nstages = graph_list[0].m_nstages
         nmicrobatches = graph_list[0].m_nmicrobatches
         nchunks = graph_list[0].m_nchunks
-        event_types = graph_list[0].event_types
+        event_types = copy.deepcopy(graph_list[0].event_types)
+        # schedule_type only exist in zbgraph, should use hasattr check
         schedule_type = None
         if hasattr(graph_list[0], 'schedule_type'):
-            g.schedule_type = graph_list[0].schedule_type
+            schedule_type = copy.deepcopy(graph_list[0].schedule_type)
 
+        '''
+            1 CHECK INPUT
+        '''
+        # Ensure that the parameters of all input graphs are consistent.
         for graph in graph_list:
             assert nstages == graph.m_nstages
             assert nmicrobatches == graph.m_nmicrobatches
@@ -21,20 +37,43 @@ class Merge():
             if not schedule_type == None:
                 assert schedule_type == graph.schedule_type
         
+        '''
+            2 BUILD EMPTY SUBGRAPH
+        '''
+        # Create a graph with both nodes and edges being empty, inheriting the parameters of the original graph.
         g = PPGraph(nstages, nmicrobatches, nchunks)
         g.event_types = event_types
         if not schedule_type == None:
             g.schedule_type = schedule_type
         
+        '''
+            3 MERGE NODES AND EDGES
+        '''
+        # Iterate through the graphs in graph._list
         for graph in graph_list:
+            # Iterate through the nodes in graph, also get its id
             for eventid, event in graph.get_nodes().items():
+                '''
+                    3.1 MERGE NODES
+                '''
+                # Ensure that there are no duplicate nodes in the graphs to be merged
                 assert eventid not in g.m_nodes.keys()
-                g.m_nodes[eventid] = event
-                if eventid in graph.m_in_edges:
-                    g.m_in_edges[eventid] = graph.m_in_edges[eventid]
-                if eventid in graph.m_out_edges:
-                    g.m_out_edges[eventid] = graph.m_out_edges[eventid]
 
+                g.m_nodes[eventid] = copy.deepcopy(event)
+
+                '''
+                    3.2 MERGE EDGES
+                '''
+                #If the node corresponding to eventid has in_edges, copy them to the new graph.
+                if eventid in graph.m_in_edges:
+                    g.m_in_edges[eventid] = copy.deepcopy(graph.m_in_edges[eventid])
+                #If the node corresponding to eventid has out_edges, copy them to the new graph.
+                if eventid in graph.m_out_edges:
+                    g.m_out_edges[eventid] = copy.deepcopy(graph.m_out_edges[eventid])
+
+        '''
+            4 ADD EDGES PROVIDED BY USERS
+        '''
         for edge in edge_list:
             g.add_edge(edge[0], edge[1])
         
@@ -42,11 +81,13 @@ class Merge():
 
 
     def merge(self, graph_list, edge_list = []):
+        # Check the input. At least one graph must be provided, and the type of all graphs must be the same.
         assert(len(graph_list) > 0)
         graph_type = type(graph_list[0])
         for graph in graph_list:
             assert(type(graph) == graph_type)
         
+        # Classify and process based on the input type.
         if graph_type == PPGraph:
             return self.merge_pp(graph_list, edge_list)
         else:
--- tests/units_tests/filter_test.py
+++ tests/units_tests/filter_test.py
@@ -9,6 +9,9 @@ from perflowai.core.event import NoneTimestamp, EventType
 from perflowai.trace_op.filter import Filter
 
 def test_Filter():
+    '''
+        0 BUILD A GRAPH
+    '''
     graph = GPipeGraph(4, 10, 1, cost_config = PipeCostConfig(
         fwd_time = 1000,
         bwd_time = 2000,
@@ -16,20 +19,29 @@ def test_Filter():
     ))
     graph.build_graph()
 
+    '''
+        1 FILTER STAGES [2,3] FROM ORIGINAL GRAPH AS SUBGRAPH
+    '''
     fil = Filter()
     target_stage_list = [2,3]
     subgraph = fil.filter(graph, target_stage_list)
 
-    #check nodes
+    '''
+        2 CHECK NODES IN SUBGRAPH
+    '''
+    # For all nodes in the original graph whose stageid in target_stage_list, they must exist in the subgraph.
     for eventid, event in graph.get_nodes().items():
         if event.m_stage_id in target_stage_list:
             assert event == subgraph.get_nodes()[eventid]
-
+    # All nodes in the subgraph must exist in the original graph, and their stageid must be in the target_stage_list.
     for eventid, event in subgraph.get_nodes().items():
         assert event == graph.get_nodes()[eventid]
         assert event.m_stage_id in target_stage_list
     
-    #check edge
+    '''
+        3 CHECK EDGES IN SUBGRAPH
+    '''
+    # For all edges in the original graph where the stageid of both endpoints belongs to target_stage_list, they must exist in the subgraph.
     for src_id in graph.m_out_edges:
         src_event = graph.get_nodes()[src_id]
         if src_event.m_stage_id in target_stage_list:
@@ -45,6 +57,7 @@ def test_Filter():
                 if dst_event.m_stage_id in target_stage_list:
                     assert dst_id in subgraph.m_in_edges[src_id]
 
+    # All edges in the subgraph must exist in the original graph
     for src_id in subgraph.m_out_edges:
         for dst_id in graph.m_out_edges[src_id]:
             assert dst_id in graph.m_out_edges[src_id]
