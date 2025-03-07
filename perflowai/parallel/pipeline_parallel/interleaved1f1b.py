6ef78536614ac4f8f95e4c7ef3d76a85cc555286
shuixirui <94513637+shuixirui@users.noreply.github.com>
2025-03-07 16:46:27 +0800

    [Test] #54 Update a integrated test, support memory footprint for offloading (#66)
    * [Test] #54 Update a integrated test, support memory footprint for offloading
    
    * [Feat] #52 Update OffReLoadEvent class, some functions are not added or implemented yet and will be refined when it is necessary.  (#65)
    
    * [Feat] #52 Update OffReLoadEvent class, some functions are not added or implemented yet and will be refined when it is necessary.
    
    * [Test] #52 Add a unit test for OffReLoadEvent class
    
    * [Feat] #53 Update class PPSimulator to support simulation on different resource (CPU, GPU, PCIE, etc.) (#67)
    
    * [Feat] #53 Update class PPsimulator to support multiple resource
    
    * [Feat] #53 Update class PPsimulator to support multiple resource
    
    * [Test] #53 Add a test for class PipeOffloadConfig
    
    * [Test] #54 Update a integrated test, support memory footprint for offloading
    
    * [Feat] #54 Update Interleaved1F1BGraph class, support offload_config
    
    * [Test] #54 Update a integrated test, support memory footprint for offloading
    
    ---------
    
    Co-authored-by: Yuyang Jin <37532408+yuyangJin@users.noreply.github.com>

--- perflowai/parallel/pipeline_parallel/interleaved1f1b.py
+++ perflowai/parallel/pipeline_parallel/interleaved1f1b.py
@@ -3,7 +3,7 @@
 '''
 
 from .ppgraph import PPGraph
-from ...core import EventType, FwdBwdEvent, NoneTimestamp
+from ...core import EventType, FwdBwdEvent, OffReLoadEvent, NoneTimestamp, ResourceType
 
 '''
 @class Interleaved1F1BGraph
@@ -12,6 +12,7 @@ An Interleaved1F1BGraph is a pipeline trace for Interleaved 1F1B.
 class Interleaved1F1BGraph(PPGraph):
     def __init__(self, nstages, nmicrobatches, nchunks, cost_config=None, offload_config=None):
         super().__init__(nstages, nmicrobatches, nchunks, cost_config)
+        self.m_offload_config = offload_config
 
     '''
     mb, chk -> minibatch id
@@ -147,11 +148,103 @@ class Interleaved1F1BGraph(PPGraph):
                         # print_dep(0, stage, mb, chk, 0, stage-1, mb, chk)
 
         '''
-            Offloading
+            OffReloading
         '''
-        if self.m_offload == True:
-            ratio = self.m_offload_config.load_ratio
-            for stage in range(self.m_nstages):
-                load_events = []
+        print(self.m_offload_config)
+        if not self.m_offload_config == None:
+            #get basic information
+            n_stages = self.get_nstages()
+            n_microbatches = self.get_nmicrobatches()
+            n_chunks = self.get_nchunks()
+            events = self.get_nodes()
+            ratio = self.m_offload_config.offload_ratio
+            for stage in range(n_stages):
+                for mb in range(n_microbatches):
+                    for chk in range(n_chunks):
+                        #get related fwd & bwd event
+                        fwd_event_id = self.get_event_id(EventType.FWD, stage, mb, chk)
+                        fwd_event = events[fwd_event_id]
+                        bwd_event_id = self.get_event_id(EventType.BWD, stage, mb, chk)
+                        bwd_event = events[bwd_event_id]
+                        #for stage==nstages - 1 & chunk == nchunks - 1, fwd and bwd excute closely, so there is no need to offload
+                        '''
+                            Offload
+                        '''
+                        if (not stage == n_stages - 1) or (not chk == n_chunks - 1):
+                            '''
+                                Generate Node
+                            '''
+                            #Get event id
+                            offl_event_id = self.get_event_id(EventType.OFFL, stage, mb, chk)
+                            #Get event num
+                            self.m_nnodes = self.m_nnodes + 1
+                            event_num = self.m_nnodes
+                            #Get event name
+                            event_name = 'OFFL' + ':' + str(mb) + '-' + str(chk)
+                            #Get duration & memory from fwd event
+                            n_warmup_minibs = (self.m_nstages * (self.m_nchunks - 1) + (self.m_nstages - stage) * 2 - 2)
+                            cur_fwd_minib_id = self.__compute_minib_id(stage, mb, chk)
+                            if cur_fwd_minib_id < n_warmup_minibs:
+                                dur = fwd_event.get_duration()
+                            else:
+                                dur = fwd_event.get_duration() + bwd_event.get_duration()
+                            mem = -1 * fwd_event.get_mem() * ratio
+                            #Create a new offl event
+                            offl_event = OffReLoadEvent(id=event_num, type=EventType.OFFL, name=event_name, timestamp=NoneTimestamp, duration=dur, 
+                                                    load_ratio=ratio, resource_type=ResourceType.G2C_PCIE,
+                                                    stage_id=stage, microbatch_id=mb, chunk_id=chk,
+                                                    mem = mem)
+                            self.m_nodes[offl_event_id] = offl_event
+                            '''
+                                Add Dependency
+                            '''
+                            self.add_edge(fwd_event_id, offl_event_id)
+                        '''
+                            Reload
+                        '''
+                        if (not stage == n_stages - 1) or (not chk == n_chunks - 1):
+                            '''
+                                Generate Node
+                            '''
+                            #Get event id
+                            offl_event_id = self.get_event_id(EventType.OFFL, stage, mb, chk)
+                            rel_event_id = self.get_event_id(EventType.REL, stage, mb, chk)
+                            #Get event num
+                            self.m_nnodes = self.m_nnodes + 1
+                            event_num = self.m_nnodes
+                            #Get event name
+                            event_name = 'REL' + ':' + str(mb) + '-' + str(chk)
+                            #Get duration & memory from fwd event
+                            n_warmup_minibs = (self.m_nstages * (self.m_nchunks - 1) + (self.m_nstages - stage) * 2 - 2)
+                            cur_bwd_minib_id = self.__compute_minib_id(stage, mb, n_chunks - chk - 1)
+                            if cur_bwd_minib_id < (self.m_nmicrobatches * self.m_nchunks - n_warmup_minibs):
+                                dur = fwd_event.get_duration() + bwd_event.get_duration()
+                            else:
+                                dur = bwd_event.get_duration()
+                            mem = fwd_event.get_mem() * ratio
+                            #Create a new offl event
+                            rel_event = OffReLoadEvent(id=event_num, type=EventType.REL, name=event_name, timestamp=NoneTimestamp, duration=dur, 
+                                                    load_ratio=ratio, resource_type=ResourceType.C2G_PCIE,
+                                                    stage_id=stage, microbatch_id=mb, chunk_id=chk,
+                                                    mem = mem)
+                            self.m_nodes[rel_event_id] = rel_event
+                            '''
+                                Add Dependency
+                            '''
+                            self.add_edge(offl_event_id, rel_event_id)
+                            self.add_edge(rel_event_id, bwd_event_id)
 
-                # TODO: issue#54 Add node and dependence for each stage @shuixirui
+
+                            if cur_bwd_minib_id < (self.m_nmicrobatches * self.m_nchunks - n_warmup_minibs):
+                                pre2_fwd_minib_id = cur_bwd_minib_id + n_warmup_minibs - 1
+                                pre2_fwd_mb, pre2_fwd_chk = self.__compute_mb_and_chk(pre2_fwd_minib_id)
+                                pre2_fwd_event_id = self.get_event_id(EventType.FWD, stage, pre2_fwd_mb, pre2_fwd_chk)
+                                self.add_edge(pre2_fwd_event_id, rel_event_id)
+                            else:
+                                pre2_bwd_minib_id = cur_bwd_minib_id - 2
+                                pre2_bwd_mb, pre2_bwd_chk = self.__compute_mb_and_chk(pre2_bwd_minib_id)
+                                pre2_bwd_event_id = self.get_event_id(EventType.BWD, stage, pre2_bwd_mb, self.m_nchunks - pre2_bwd_chk - 1)
+                                self.add_edge(pre2_bwd_event_id, rel_event_id)
+
+
+
--- tests/integration_tests/memory_footprint_test.py
+++ tests/integration_tests/memory_footprint_test.py
@@ -2,9 +2,10 @@
 test Generate pipeline & Visualize the memory footprint of the simulated trace
 '''
 
-from perflowai.parallel.pipeline_parallel import PipeCostConfig, GPipeGraph, PipeDreamGraph, Interleaved1F1BGraph, ZeroBubbleGraph, ScheduleType
+from perflowai.parallel.pipeline_parallel import PipeCostConfig, PipeOffloadConfig, GPipeGraph, PipeDreamGraph, Interleaved1F1BGraph, ZeroBubbleGraph, ScheduleType
 from perflowai.simulator import PPSimulator, PipeType
 from perflowai.visualizer import MemoryFootprintVisualizer
+from perflowai.core import OffReLoadEvent, EventType, ResourceType
 
 def test_GPipe_Memory_Footprint_Visualize():
 
@@ -108,6 +109,29 @@ def test_ZBV_Memory_Footprint_Visualize():
     nchunks = 2
 
     g = ZeroBubbleGraph(nstages, nmicrobatches, nchunks, cost_config = config, schedule_type = ScheduleType.ZBV)
+    g.build_graph()
+
+    sim = PPSimulator(PipeType.Interleaved1F1B, g)
+    trace = sim.run()
+
+    mfp = trace.get_memory_foorprint()
+
+    MemoryFootprintVisualizer(mfp).visualize()
+
+
+def test_offloading_Memory_Footprint_Visualize():
+
+    config = PipeCostConfig(fwd_time = 1314, 
+                            bwd_time = 2439,
+                            fwd_mem = 27.0,
+                            bwd_mem = -27.0 
+                            )
+    poc = PipeOffloadConfig(offload_ratio = 0.5)
+    nstages = 4
+    nmicrobatches = 10
+    nchunks = 2
+
+    g = Interleaved1F1BGraph(nstages, nmicrobatches, nchunks, cost_config = config, offload_config = poc)
     g.build_graph()
 
     sim = PPSimulator(PipeType.Interleaved1F1B, g)
