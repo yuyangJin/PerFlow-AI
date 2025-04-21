3662e49511cb2e9ddd8fbad9f5cdeb0c69bbc87c
yuyangJin <yuyuanjin47@gmail.com>
2025-04-21 08:37:39 +0800

    [Feat] #90 Update core module __init__

--- perflowai/core/__init__.py
+++ perflowai/core/__init__.py
@@ -2,7 +2,11 @@
 @module perflowai.core
 '''
 
-from .event import EventType, ResourceType, Event, OprtEvent, CommEvent, FwdBwdEvent, OffReLoadEvent, NoneTimestamp, NoneMem
+from .event import EventType, ResourceType, Event, OprtEvent, CommEvent, FwdBwdEvent, OffReLoadEvent, NoneTimestamp, NoneMem, RequestEvent, ScheduleEvent, PrefillEvent, DecodeEvent
 from .trace import Trace, PPTrace
+from .request import Request, Task, TaskType, Tasks, TaskPool
+from .scheduler import Scheduler
+from .device import Device, DeviceType
 
-__all__ = ['EventType', 'ResourceType', 'OprtEvent', 'CommEvent', 'FwdBwdEvent', 'OffReLoadEvent']
+
+__all__ = ['EventType', 'ResourceType', 'OprtEvent', 'CommEvent', 'FwdBwdEvent', 'OffReLoadEvent', 'Request','Task', 'TaskType', 'Tasks', 'TaskPool', 'Scheduler']
